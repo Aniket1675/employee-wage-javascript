4a3f4e9828a5e3e8296457b03867329dd2dfe182
Aniket1675 <Aniket@05>
2022-04-26 15:01:50 +0530

    AADED:Check employee is present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,12 @@
-console.log("Welcome to Employee Wage Program using Javascript");
+//Uc-1 Check Employee is Present Or Absent
+
+let empCheck = Math.floor(Math.random() * 10) % 3;
+console.log("UC-1")
+const IS_ABSENT = 0;
+if(empCheck == IS_ABSENT){
+    console.log("Employee is Absent");
+    console.log("_______________________________________________");
+}else{
+    console.log("Employee is Present");
+    console.log("_______________________________________________");
+}
